47d5caf3d603b3628e76a833d9b038fe812a5258
Carlos Balladares <carlos@balladares.co>
2018-08-29 01:10:51 -0700

    added preset model

---
+++ src/main/java/com/carlosbf/presetvault/model/Preset.java
@@ -0,0 +1,53 @@
+package com.carlosbf.presetvault.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+public class Preset {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private User    owner;
+    private String  fileURL;
+    private Pack    pack;
+
+    public Preset(String fileURL) {
+        this.fileURL = fileURL;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getFileURL() {
+        return fileURL;
+    }
+
+    public void setFileURL(String fileURL) {
+        this.fileURL = fileURL;
+    }
+
+    public Pack getPack() {
+        return pack;
+    }
+
+    public void setPack(Pack pack) {
+        this.pack = pack;
+    }
+}
